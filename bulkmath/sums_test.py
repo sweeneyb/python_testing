c29f464dd5188480c52379d9173d68233d86400f
sweeneyb <tab126@gmail.com>
2022-07-06 23:36:17 -0400

    add a paramtrized test

--- bulkmath/sums_test.py
+++ bulkmath/sums_test.py
@@ -1,4 +1,13 @@
 import bulkmath
+import pytest
+
+@pytest.mark.parametrize("x, y, output",[(1,3,4), (5,5,10)])
+def test_more_sums(x, y, output):
+    assert bulkmath.sum(x,y) == output
+
 
 def test_sum():
     assert bulkmath.sum(1,2) == 3
+
+def test_tuples():
+    assert bulkmath.map_sum_tuples([(1,3), (5,7)]) == [ 4, 12]
